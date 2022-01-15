41f5c727d977fd3a8c06e726de5a4619612bb6bf
faridrh <11236895+faridrh@users.noreply.github.com>
2022-01-15 23:57:22 +0400

    first version

---
+++ print_pretty.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while(T--) {
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+
+		int T;
+        cin >> T;
+        float n[3];
+        // receive all elements of array
+        for (int i=0; i<3; i++) {
+        cin >>n[i];
+        }
+        int i;
+        float n_new[3];
+        switch (i){
+            case 1:
+                n_new[0]=n[0] ;
+        }
+    
+        }
+	}
