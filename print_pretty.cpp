631f08b222f4a775a73ee0a3820ca8f2791494e2
Farid Rahimli <11236895+faridrh@users.noreply.github.com>
2022-02-05 13:08:38 +0000

    Adding cout format, beatifying

--- print_pretty.cpp
+++ print_pretty.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
-#include <iomanip> 
+
+#include <iomanip>
+
 using namespace std;
 
 int main() {
-	int T; cin >> T;
-	cout << setiosflags(ios::uppercase);
-	cout << setw(0xf) << internal;
-	while(T--) {
-		double A; cin >> A;
-		double B; cin >> B;
-		double C; cin >> C;
-        unsigned long  Ax;
-        char s[32],s2[32];
-        sprintf(s,"%fA",A);
-        Ax= atof(s);
-       //cout.setf ( ios::showbase ); 
-       cout.unsetf(ios::uppercase);// unset uppercase
-        cout << setw(0xf)<<left; // align to left
-        cout <<hex<<Ax<< endl; 
-        cout << setfill ('_')<< setw(15) << B;
-        //sprintf(s2,"%+15.2f",B);
-       /* for (int i =0; i<15;i++){
-            if (s2[i]==' '){
-                s2[i]='_';
-            }
-        }*/
-        printf("%s\n",s2);
-        printf("%.9E\n",C);
-	}
-	return 0;
+    int T;
+    cin >> T;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
+    while (T--) {
+        double A;
+        cin >> A;
+        double B;
+        cin >> B;
+        double C;
+        cin >> C;
+        unsigned long Ax;
+        char s[32];
+        sprintf(s, "%fA", A);
+        Ax = atof(s);
+        cout.setf(ios::showbase);
+        cout.unsetf(ios::uppercase); // unset uppercase
+        cout << setfill(' ') << setw(0xf) << left; // align to left
+        cout << hex << Ax << endl; // print first number
+        cout << setfill('_') << setw(15) << right << setprecision(2) << fixed << showpos << B << endl;
+        printf("%.9E\n", C); // keeping as printf to show example
+    }
+    return 0;
 
 }
