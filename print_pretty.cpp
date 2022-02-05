3e247440c7ba9260bb2ab434477f04e9c6fd1fe0
fariddigi <>
2022-02-05 12:00:51 +0000

    IOmanip

--- print_pretty.cpp
+++ print_pretty.cpp
@@ -14,16 +14,17 @@ int main() {
         char s[32],s2[32];
         sprintf(s,"%fA",A);
         Ax= atof(s);
-       cout.setf ( ios::showbase ); 
-       cout.unsetf(ios::uppercase);
-        cout << setw(0xf)<<left;
-        cout <<hex<<Ax<< endl;
-        sprintf(s2,"%+15.2f",B);
-        for (int i =0; i<15;i++){
+       //cout.setf ( ios::showbase ); 
+       cout.unsetf(ios::uppercase);// unset uppercase
+        cout << setw(0xf)<<left; // align to left
+        cout <<hex<<Ax<< endl; 
+        cout << setfill ('_')<< setw(15) << B;
+        //sprintf(s2,"%+15.2f",B);
+       /* for (int i =0; i<15;i++){
             if (s2[i]==' '){
                 s2[i]='_';
             }
-        }
+        }*/
         printf("%s\n",s2);
         printf("%.9E\n",C);
 	}
